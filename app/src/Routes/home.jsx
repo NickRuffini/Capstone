ee9b4cba9cf3a9b753a33fe6aa34fc6435cd0586
NickRuffini <ruffini@ksu.edu>
2022-03-03 15:54:56 -0600

    Changed UI elements for Home page

--- app/src/Routes/home.jsx
+++ app/src/Routes/home.jsx
@@ -1,5 +1,20 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
 export default function Home() {
     return(
-        <main style={{ padding: "1rem 0" }}> Hi </main>
+        <main style={{ padding: "1rem 0" }}>
+        <h2>General Takeaways</h2>
+        <Grid container spacing={2} alignItems="center" justifyContent="center">
+          <Grid item xs={3}>
+            <Dropdown placeholder="Countries" />
+          </Grid>
+          <Grid item xs={3}>
+            <Dropdown placeholder="Criteria"/>
+          </Grid>
+        </Grid>
+      </main>
     );
 }
